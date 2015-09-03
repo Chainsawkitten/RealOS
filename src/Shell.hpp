b601173b154b915f2605328c8aa75acb2d0657b6
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2015-09-03 16:12:01 +0200

    Doxygen comments on shell.cpp

--- src/Shell.hpp
+++ src/Shell.hpp
@@ -3,14 +3,33 @@
 
 #include <string>
 #include <vector>
-
-/* Takes usercommand from input and returns number of commands, commands are stored in strArr[] */
+///Takes usercommand from input and returns number of commands, commands are stored in strArr[]
+/**
+@param userCommand the command the user wants to execute
+@param strArr[] array that stores commands
+@return the number of commands
+*/
 int parseCommandString(const std::string &userCommand, std::string strArr[]);
+
+///
+/**
+@param command command string
+@return the index number of the command, -1 if command could not be found
+*/
 int findCommand(std::string &command);
 
-/* Splits a filepath into multiple strings */
+///Splits a filepath into multiple strings
+/**
+@param filePath string of filepath to split.
+@param delim character that delimits strings
+@return vector of strings that was split from filepath
+*/
 std::vector<std::string> split(const std::string &filePath, const char delim = '/');
 
+/// Returns help-string
+/**
+@return returns a string with helpful information
+*/
 std::string help();
 
 #endif // SHELL_HPP
